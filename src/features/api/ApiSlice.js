0ad3fe88e46a7eadc491f2649735a17d437ef182
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-10-28 09:33:48 +0600

    video added successfully

--- src/features/api/ApiSlice.js
+++ src/features/api/ApiSlice.js
@@ -1,27 +1,35 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const ApiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:9000'
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000",
+  }),
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: () => "/videos",
+      keepUnusedDataFor: 30,
     }),
-    endpoints: (builder)=> ({
-        getVideos: builder.query({
-            query: () => '/videos'
-        }),
-        getVideo: builder.query({
-            query: (videoId) => `/videos/${videoId}`
-        }),
-        getRelatedVideos: builder.query({
-            query: ({id, title}) => {
-                const tags = title.split(" ")
-                const likes = tags.map((tag) => `title_like=${tag}`)
-                const queryString = `/videos?${likes.join('&')}&_limit=4`
-                return queryString;
-
-            }
-        })
+    getVideo: builder.query({
+      query: (videoId) => `/videos/${videoId}`,
     }),
-})
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split(" ");
+        const likes = tags.map((tag) => `title_like=${tag}`);
+        const queryString = `/videos?${likes.join("&")}&_limit=4`;
+        return queryString;
+      },
+    }),
+    addVideo: builder.mutation({
+      query: (data) => ({
+        url: `/videos`,
+        method: 'POST',
+        body: data
+      }),
+    }),
+  }),
+});
 
-export const {useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery} = ApiSlice
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery, useAddVideoMutation } =
+  ApiSlice;
